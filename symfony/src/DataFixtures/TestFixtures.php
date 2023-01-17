4912d0164bb481856fe5ff1e4c8e59cd676ad62c
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2023-01-17 19:29:54 +0500

    test fixtures created

---
+++ symfony/src/DataFixtures/TestFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Section;
+use App\Entity\Test;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class TestFixtures extends BaseFixtures implements DependentFixtureInterface
+{
+
+    function loadData(ObjectManager $manager)
+    {
+        $this->createMany(Test::class, 100, function (Test $test) use ($manager) {
+            $test
+                ->setTitle($this->faker->colorName . '-'. $this->faker->company)
+                ->setSection($this->getRandomReference(Section::class));
+        });    }
+
+    public function getDependencies(): array
+    {
+        return [
+            SectionFixtures::class,
+        ];
+    }
+}
