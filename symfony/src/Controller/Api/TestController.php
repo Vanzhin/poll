3e516d5c68d8cd0e3abe628677304bd4cf6da9ef
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2023-02-01 15:34:25 +0500

    fix

--- symfony/src/Controller/Api/TestController.php
+++ symfony/src/Controller/Api/TestController.php
@@ -2,17 +2,13 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\Question;
 use App\Entity\Test;
 use App\Repository\TestRepository;
 use App\Service\QuestionService;
 use Doctrine\DBAL\Exception;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
@@ -68,7 +64,7 @@ class TestController extends AbstractController
         foreach ($data as $answerData) {
             $response[] = $questionService->handle($answerData);
         }
-        
+
         return $this->json(['questions' => $response],
             200,
             ['charset=utf-8'],
