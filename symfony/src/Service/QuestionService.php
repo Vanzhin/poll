882e40356670e340df054a6264ecbfe651b167ab
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2023-02-02 16:50:09 +0500

    radio, checkbox

--- symfony/src/Service/QuestionService.php
+++ symfony/src/Service/QuestionService.php
@@ -7,48 +7,86 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class QuestionService
 {
+    const SHUFFLED_VARIANTS = 'shuffled';
 
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(private readonly EntityManagerInterface $entityManager, private readonly SessionService $sessionService)
     {
     }
 
     public function handle(array $answerData): array
     {
-        $question = $this->entityManager->find(Question::class, $answerData["id"]);
-        if ($question) {
-            return [
-                "id" => $answerData["id"],
-                "title" => $question->getTitle(),
-                "variant" => $question->getVariant(),
-                "result" => [
-                    "true_answer" => $question->getAnswer(),
-                    "user_answer" => $answerData["answer"],
-                    "score" => $this->score($question, $answerData["answer"])
+        try {
+            $question = $this->entityManager->find(Question::class, $answerData["id"]);
+            if ($question) {
+                $response = [
+                    "id" => $answerData["id"],
+                    "title" => $question->getTitle(),
+                    "variant" => $question->getVariant(),
+                    "result" => [
+                        "true_answer" => $this->getShuffledAnswers($question),
+                        "user_answer" => $answerData["answer"],
+                        "score" => $this->getQuestionScore($question, $answerData["answer"])
+                    ],
+                    "type" => $question->getType()
+                ];
+            } else {
+                $response = [
+                    "id" => $answerData["id"],
+                    "error" => "question not found"
+                ];
+            }
 
-
-                ]
-
-            ];
+        } catch (\Exception $e) {
+            $response = ["error" => $e->getMessage()];
+        } finally {
+            $this->sessionService->remove(self::SHUFFLED_VARIANTS);
+            return $response;
         }
-        return [
-            "id" => $answerData["id"],
-            "error" => "question not found"
-        ];
-
-
-//     получаю
-//     {"id":"1001", "answer":["quia","odio","aut","unde"]}
-//     отдать
-//     {"id":"1001", "variant":["quia","odio","aut","unde"], "result":{"true_answer": ["odio"], "user_answer":["unde"], "score": "0" }}
-
 
     }
 
-    private function score(Question $question, array $answerData): bool
+    public function shuffleVariants(array $questions): array
+    {
+        foreach ($questions as $question) {
+            if (count($question->getVariant()) > 1) {
+                $variants = $question->getVariant();
+                shuffle($variants);
+                $question->setVariant($variants);
+                $this->sessionService->add([$question->getId() => $variants], self::SHUFFLED_VARIANTS);
+            }
+        }
+        return $questions;
+    }
+
+    private function getShuffledAnswers(Question $question, string $name = self::SHUFFLED_VARIANTS): array
+    {
+//        $testSession = [
+//            6885 => ["sunt", "doloremque", "ipsa", "sint"],
+//            6321 => ["eum", "at", "reiciendis"],
+//            6514 => ["et", "debitis", "molestiae", "debitis", "recusandae"]
+//        ];
+        if ($this->sessionService->get($name) && array_key_exists($question->getId(),$this->sessionService->get($name))) {
+//        if (array_key_exists($question->getId(), $testSession)) {
+
+            $shuffledVariants = $this->sessionService->get($name)[$question->getId()];
+//            $shuffledVariants = $testSession[$question->getId()];
+
+            $answers = [];
+            foreach ($question->getAnswer() as $answer) {
+                $answers[] = array_search($question->getVariant()[$answer], $shuffledVariants);
+            }
+
+        } else {
+            $answers = $question->getAnswer();
+        }
+        return $answers;
+    }
+
+    private function getQuestionScore(Question $question, array $answerData): bool
     {
         $score = false;
         $userAnswers = $this->answerPrepare($answerData);
-        $questionAnswers = $this->answerPrepare($question->getAnswer());
+        $questionAnswers = $this->answerPrepare($this->getShuffledAnswers($question));
         $questionVariants = $this->answerPrepare($question->getVariant());
 
         switch ($question->getType()->getTitle()) {
@@ -59,6 +97,10 @@ class QuestionService
 
                 break;
             case 'checkbox':
+                if (count(array_diff($questionAnswers, $userAnswers)) === 0) {
+                    $score = true;
+                }
+                break;
             case 'checkbox_picture':
             case 'input_many':
             case 'blank':
@@ -76,12 +118,6 @@ class QuestionService
                 }
                 break;
 
-//            case 'textarea':
-//                $question->setTitle($this->faker->realTextBetween(10, 50) . '?')
-//                    ->setVariant([])
-//                    ->setAnswer([])
-//                    ->addTicket($this->getRandomReference(Ticket::class));
-//                break;
         }
         return $score;
     }
@@ -92,4 +128,5 @@ class QuestionService
             return mb_strtolower(trim($answer));
         }, $answers);
     }
+
 }
