55135e78f509a7c38c7d9db62d397070c9e29f4f
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2023-02-25 16:13:07 +0300

    админ панель

---
+++ symfony/assets/layouts/AdminLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <Headers/>
+  <div class="container">
+    <div class="wrapper">
+      <div class="head"></div>
+    </div>
+    <div class="wrapper">
+      <RouterView />
+    </div>
+  </div>
+</template>
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import Headers from '../components/Headers.vue'
+export default {
+  components: {
+    Headers,
+  },
+} 
+</script>
+<style lang="scss">
+.head{
+  height: 80px;
+}
+
+</style>
