5a1f92dfe85aecb3235171d4a04dc2bd9cdcd333
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2023-04-12 10:05:35 +0500

    count question started

---
+++ symfony/migrations/Version20230411140124.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230411140124 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE section ADD question_count_to_pass SMALLINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE test ADD section_count_to_pass SMALLINT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE section DROP question_count_to_pass');
+        $this->addSql('ALTER TABLE test DROP section_count_to_pass');
+    }
+}
