1c6d5ef7192fc60828dbf9e657f0a93ae732b273
Aleksej-F <l7933@yandex.ru>
2023-07-23 12:49:59 +0300

    fix

--- test_nuxt/utils/constApi.js
+++ test_nuxt/utils/constApi.js
@@ -1,2 +1,3 @@
 // export const urlApi = 'https://test.9784023.ru'
-export const urlApi = 'http://test2-open'
+// export const urlApi = 'http://test2-open'
+export const urlApi = 'https://192.168.0.103'
