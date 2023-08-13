72ee8ca0627abd77f015bdfb4eb81b53fc3ac343
Aleksej-F <72097093+Aleksej-F@users.noreply.github.com>
2023-08-13 18:30:04 +0300

    главная страница

---
+++ test_nuxt/pages/admin/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="block">
+    <div class="title">
+      Вы находитесь на странице администратора.
+    </div>
+   <div class="container">
+      <div class="row">
+        
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<script setup>
+ definePageMeta({
+    layout: "admin",
+    middleware: 'auth'
+  });
+  
+ 
+</script>
+<style lang="scss" scoped>
+  .title{
+    margin-top: 60px;
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 40px;
+    color: #269EB7;
+  }
+  .button{
+    padding: 5px 10px;
+    transition: all 0.1s ease-out;
+    &:hover{
+      background-color: rgb(156, 156, 154);
+    }
+  }
+ @media (min-width: 1024px) {
+  
+ }
+</style>
