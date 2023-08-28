d4bad10fe576eebbc88f4f267a1716fbbe639a9a
Andrew Leshkevich <magicgts@gmail.com>
2023-08-28 19:36:53 +0500

    Update constApi.js
    Define in .env variable like:
    API_URL=https://test.9784023.ru

--- test_nuxt/utils/constApi.js
+++ test_nuxt/utils/constApi.js
@@ -1,3 +1,2 @@
-// export const urlApi = 'https://test.9784023.ru'
-// export const urlApi = 'http://test2-open'
-export const urlApi = 'https://192.168.0.103'
+import "dotenv";
+export const urlApi = "process.env.API_URL";
