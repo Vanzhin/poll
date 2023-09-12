75490616ade1c43558b704896f64816f1e5432e2
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2023-09-13 01:29:23 +0300

    Alex ss radmin6 (#137)
    * Регистрация
    
    * страница тесты админка
    
    * импорт
    
    * авторизация по ссылке
    
    * авторизация по ссылке
    
    * страница компании

---
+++ test_nuxt/components/Redirect.client.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="bagraund">
+    <Head>
+      <Title>Авторизация | Амулет Тест</Title>
+      <Meta name="robots" content="noindex"/>
+      
+    </Head>
+   <section class="wrapper">
+    <div 
+      class="login-page"
+    >
+    <div class="login-page-text">Поздравляем!</div>
+      <p>Вы успешно авторизовались. 
+        И скоро будете перенаправлены на сайт. 
+        Если этого не произойдет, кликните по ссылке. </p>
+        <NuxtLink :to='user.getPageName'> Обратно на сайт</NuxtLink>
+    </div>
+  </section>
+</div>
+</template>
+ 
+<script setup>
+  
+  import { useUserStore  } from '@/stores/UserStore'
+  const user = useUserStore()
+  const route = useRoute()
+  const rtoken = ref(route.params.rtoken ? route.params.rtoken: false)
+ 
+  onMounted(async() => {
+      if ( rtoken ) {
+        await user.getAuthRefresh(rtoken.value)
+        await user.savePageIsLocalStorage()
+      }
+      setTimeout(() => {
+        navigateTo( user.getPageName )
+      },15000);
+    })
+  
+</script>
+
+<style lang="scss" scoped>
+.bagraund{
+  position: fixed;
+  top:0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #585858;
+  z-index: 30;
+}
+.login-page {
+  max-width: 380px;
+  padding: 24px;
+  background: #cad5d5;
+  box-shadow: 0px 4px 15px rgba(34, 42, 70, 0.08);
+  border-radius: var(--radius);
+  margin: 100px auto;
+  &-text{
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 26px;
+    text-align: center;
+  }
+}
+@media (min-width: 1024px) {
+  
+}
+</style>
